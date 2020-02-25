e4f72f047a3cee7207daee73287fd48c7b823266
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2020-02-25 15:18:57 +0630

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   requestify = require('requestify'),
-  firebase = require(firebase-admin),
+  firebase = require('firebase-admin'),
   app = express().use(bodyParser.json()); // creates express http server
 
   const pageaccesstoken = process.env.PAGE_ACCESS_TOKEN;
