7e297e604d681251df34b0221aedf57190b7e824
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2020-03-03 15:12:07 +0630

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.post('/webhook', (req, res) => {
 
 
 
-        if(userInput && askUserName == true){
+        if((userInput || userButton) && askUserName == true){
            let data = {
             "name": userInput,
           
