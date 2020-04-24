1163bfa7fe1f030c5f830dcb09a7c80cf0608014
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2020-04-24 13:56:28 +0630

    Update index.js

--- index.js
+++ index.js
@@ -613,7 +613,7 @@ const showBookingNumber = (sender_psid, ref) => {
       "id":sender_psid,
     },           
     "message":{
-      "text": `You arn't ok when you going to trip text this message (change update:reference no) ${ref}`,              
+      "text": `You aren't ok when you going to trip text this message (change update:reference no) and your reference is ${ref}`,              
     }
   } 
 
