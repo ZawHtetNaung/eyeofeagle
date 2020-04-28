34b1769376239bdb20756b72e0672d8776b5cd29
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2020-04-28 21:31:35 +0630

    Update index.js

--- index.js
+++ index.js
@@ -610,14 +610,13 @@ app.post('/webhook', (req, res) => {
         send(welcomeMessage);
       } */
 
-        If(userInput.includes("Change package:")){
+        if(userInput.includes("Change package:")){
           console.log('USER INPUT',userInput);
           let ref_num = userInput.slice(15);
           ref_num = ref_num.trim();
           console.log('REF NUM',ref_num); 
           update_package(sender_psid,ref_num); 
-
-        }
+      }
 
 
       /*if(userInput == "Change package" || quickdata == "Change package"){
