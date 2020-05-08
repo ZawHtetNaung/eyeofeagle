16ece7b0a8cbb1ac79dd0d1c59bc4d53b328d005
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2020-05-08 20:45:26 +0630

    Update index.js

--- index.js
+++ index.js
@@ -277,6 +277,10 @@ app.post('/parks_customize', function(req, res) {
     let hotel = req.body.hotel;
     let name = req.body.name;
     let mobile = req.body.mobile;
+    let company = req.body.company;
+    let e_mail = req.body.e_mail;
+    let arriveddate = req.body.arriveddate;
+    let arrivedtime = req.body.arrivedtime;
     let sender = req.body.sender;
 
     let booking_ref = generateRandom(5);
@@ -291,6 +295,10 @@ app.post('/parks_customize', function(req, res) {
         hotel: hotel,
         name: name,
         mobile: mobile,
+        company: company,
+        e_mail: e_mail,
+        arriveddate: arriveddate,
+        arrivedtime: arrivedtime,
         booking_ref: booking_ref,
     }).then(success => {
         console.log("DATASAVESHOWBOOKINGNUMBER");
